4d2b481dfca55892203cd1b8663599a6ea7d05a1
Jona <jona.oestreicher@gmail.com>
2025-04-23 16:17:15 +0200

    updated installation script

--- install_simsalabim.py
+++ install_simsalabim.py
@@ -2,6 +2,19 @@ import os
 
 from pySIMsalabim.install.get_SIMsalabim import install_SIMsalabim
 
-cwd = os.getcwd()
-install_SIMsalabim(cwd)
+
+def main():
+    fpc_path = "/path/to/fpc/bin/x86_64-linux" # Change this to the actual path where fpc is installed
+    if not os.path.exists(fpc_path):
+        print("No Free Pascal Compiler (FPC) found at the specified path. Trying to run installation without it. FPC might be installed during the installation of SIMsalabim.")
+
+    original_path = os.getenv("PATH", "")
+    combined_path = f"{fpc_path}{os.pathsep}{original_path}"
+    os.environ["PATH"] = combined_path
+
+    cwd = os.getcwd()
+    install_SIMsalabim(cwd)
+
+if __name__ == "__main__":
+    main()
 
